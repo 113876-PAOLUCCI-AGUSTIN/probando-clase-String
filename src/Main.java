07bc05fe5eaf46d0aa72325a6de5524ef8343e6e
113876-PAOLUCCI-AGUSTIN <113876@tecnicatura.frc.utn.edu.ar>
2024-03-27 12:50:46 -0300

    Subject modifications.

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,10 @@
 /*
-*  SOURCE: https://docs.oracle.com/en/java/javase/17/docs/api/java.base/java/lang/String.html
-* tip Intellij Idea: CTRL+A abre link en navegador predeterminado.
-*/
+ * SOURCE: https://docs.oracle.com/en/java/javase/17/docs/api/java.base/java/lang/String.html
+ * tip Intellij Idea: CTRL+A abre link en navegador predeterminado.
+ *
+ */
+
+import java.sql.SQLOutput;
 
 public class Main {
     public static void main(String[] args) {
@@ -36,41 +39,56 @@ public class Main {
         // Probando diferentes métodos de un objeto de tipo String.
         String citaSaramago = "Es una estupidez perder el presente solo por el miedo de no llegar a ganar el futuro. \n";
 
-        System.out.println("----- LONGITUD -----");
+        System.out.println("----- LONGITUD // LENGTH -----");
         System.out.println(citaSaramago.length());           // INT: Indica la LONGITUD. Obvio que incluye espacios.
+        System.out.println(" ");
 
-        System.out.println("----- CONTIENE -----");
+
+        System.out.println("----- CONTIENE // CONTAINS -----");
         System.out.println(citaSaramago.contains("miedo"));  // BOOLEAN: Indica si CONTIENE un character o cadena.
+        System.out.println(" ");
 
-        System.out.println("----- REPEAT -----");
+
+        System.out.println("----- REPETIR CANTIDAD DE VECES // REPEAT -----");
         System.out.println(citaSaramago.repeat(22));    // REPITE la cadena.
+        System.out.println(" ");
 
-        System.out.println("----- UPPER LOWER CASE -----");
+
+        System.out.println("----- MAYUSCULAS-MINUSCULAS // UPPER-LOWER CASE -----");
         String fraseCorta = "La vida es corta";
         System.out.println(fraseCorta.toLowerCase());        // Convierte todos los caracteres a MINÚSCULAS.
         System.out.println(fraseCorta.toUpperCase());        // Convierte todos los caracteres a MAYÚSCULAS.
+        System.out.println(" ");
 
-        System.out.println("----- TRIM -----");
+
+        System.out.println("----- QUITA ESPACIOS // TRIM -----");
         fraseCorta = "       La vida es corta    ";
         System.out.println(fraseCorta);
         System.out.println(fraseCorta.trim());               // Quita espacios en blando delante y atrás.
         fraseCorta = fraseCorta.trim();
+        System.out.println(" ");
 
-        System.out.println("----- CHARAT -----");
+
+        System.out.println("----- CARACTER EN POSICION // CHARAT -----");
         System.out.println(fraseCorta.charAt(5));            // Te devuelve que caracter esta en una det posición. Empieza en CERO.
-
-        System.out.println("----- GETCLASS -----");
+        System.out.println(" ");
+        System.out.println("----- OBTENER CLASE // GETCLASS -----");
         System.out.println(fraseCorta.getClass());           // Como es un objeto permite ver de que clase viene.
+        System.out.println(" ");
+
 
         System.out.println("----- INDEXOF -----");
         System.out.println(fraseCorta.indexOf("es",1));
+        System.out.println(" ");
 
-        System.out.println("----- CONCAT -----");
+
+        System.out.println("----- CONCATENADOR // CONCAT -----");
         String calle = "Chacabuco 550 ";
         String ciudad = "Córdoba ";
         String pais = "Argentina ";
         String domicilioCompleto = calle.concat(ciudad).concat(pais);
         System.out.println(domicilioCompleto);
         System.out.println("------------------");
+        System.out.println(" ");
     }
 }
